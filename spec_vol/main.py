ff669a01be94b4b3ded8f257b339c694eecbe541
ShenzhenLime <2752234678@qq.com>
2026-03-27 15:14:46 +0800

    增加Rank IC 半衰期的计算和绘图

--- spec_vol/main.py
+++ spec_vol/main.py
@@ -4,7 +4,7 @@
 """
 
 from quant_infra.get_data import get_stock_data_by_date, get_daily_basic
-from quant_infra.factor_analyze import evaluate_factor, group_plot
+from quant_infra.factor_analyze import evaluate_factor, group_plot, ic_plot, ic_half_life
 from quant_infra.factor_calc import calc_resid, calc_spec_vol
 from quant_infra.trade import simulate_trade
 from datetime import datetime
@@ -37,5 +37,7 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    group_plot('全市场', '月度', 'short', factor_table)
+    # group_plot('全市场', '月度', 'short', factor_table)
+    # ic_plot('全市场', '月度', factor_table)
+    ic_half_life(factor_table=factor_table, max_lag=200)
     
